3474c36a0733ae9c805bd2ea204c595256b4e6c1
c4w <maezbro8@gmail.com>
2025-12-23 02:42:57 +0300

    -<script>

--- cat.js
+++ cat.js
@@ -1,11 +1,7 @@
-<script>
 document.documentElement.innerHTML = "";
 const app = document.createElement("div");
 app.id = "app";
 app.innerHTML = `
     <h1>hello</h1>
     <p>for xd</p>
-`;
-
-document.documentElement.appendChild(app);
-</script>
+`;document.documentElement.appendChild(app);
