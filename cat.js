c650d7fb746f36cebcf3b4dbe1f0704ca7978211
c4w <maezbro8@gmail.com>
2025-12-23 01:15:07 +0300

    base

---
+++ cat.js
@@ -0,0 +1,11 @@
+<script>
+document.documentElement.innerHTML = "";
+const app = document.createElement("div");
+app.id = "app";
+app.innerHTML = `
+    <h1>hello</h1>
+    <p>for xd</p>
+`;
+
+document.documentElement.appendChild(app);
+</script>
